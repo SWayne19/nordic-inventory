3a9a8ada2c4ddd4eeda786aa2d95d09bc3433799
SWayne19 <154661835+SWayne19@users.noreply.github.com>
2026-05-03 23:15:57 +0630

    refector : reduce paginate count and comments refector

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -10,69 +10,73 @@ use App\Models\Order;
 
 class OrderController extends Controller
 {
+    // get paginated orders for auth user
     public function index()
     {
         $orders = Order::with('items.product')
                 ->where('user_id', auth()->id())
                 ->latest()
-                ->paginate(10);
+                ->paginate(2);
 
         return response()->json($orders);
     }
 
+    // store new orders
     public function store(Request $request)
     {
+        // validation
         $request->validate([
             'items' => 'required|array',
             'items.*.product_id' => 'required|exists:products,id',
             'items.*.quantity' => 'required|integer|min:1',
         ]);
 
+        // database transaction for safe store
         return DB::transaction(function () use ($request) {
             $totalPrice = 0;
             $orderItemsData = [];
 
             foreach ($request->items as $item) {
-                //prevent race condition
+                // prevent race condition
                 $product = Product::lockForUpdate()->find($item['product_id']);
 
+                // stock check
                 if ($product->stock < $item['quantity']) {
                     return response()->json([
                         'message' => "Product {$product->name} is out of stock."
                     ], 400);
                 }
 
-                // total price calculation
+                // calculate total price
                 $itemTotal = $product->price * $item['quantity'];
                 $totalPrice += $itemTotal;
 
-                // add into orderitem array
                 $orderItemsData[] = [
                     'product_id' => $product->id,
                     'quantity' => $item['quantity'],
                     'price' => $product->price,
                 ];
 
-                // balance update
+                // reduce balance
                 $product->decrement('stock', $item['quantity']);
-
-                // store in order table
-                $order = Order::create([
-                    'user_id' => auth()->id(),
-                    'total_price' => $totalPrice,
-                ]);
-
-                // store order items
-                foreach ($orderItemsData as $data) {
-                    $order->items()->create($data);
-                }
-
-                return response()->json([
-                    'message' => 'Order placed successfully!',
-                    'order_id' => $order->id,
-                    'total_price' => $totalPrice
-                ], 201);
             }
+
+            // create order
+            $order = Order::create([
+                'user_id' => auth()->id(),
+                'total_price' => $totalPrice,
+            ]);
+
+            // save orderitems
+            foreach ($orderItemsData as $data) {
+                $order->items()->create($data);
+            }
+
+            return response()->json([
+                'message' => 'Order placed successfully!',
+                'order_id' => $order->id,
+                'total_price' => $totalPrice
+            ], 201);
         });
     }
 }
