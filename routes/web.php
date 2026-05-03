65f97435a88b93c2abc3da719d24c9d1ce7e630c
SWayne19 <154661835+SWayne19@users.noreply.github.com>
2026-05-03 23:19:04 +0630

    feat : add fallback route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// root route that load the application
 Route::get('/', function () {
+    return view('welcome');
+});
+
+// fallback for undefined urls
+Route::fallback(function () {
     return view('welcome');
 });
